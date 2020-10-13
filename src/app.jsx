59df432650fd838951218a7450acb866790b451b
elmarek <mareks@gmx.com>
2020-10-13 15:02:30 -0700

    My Site Init

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import React, { Component } from 'react'
+
+export default class app extends Component {
+  render() {
+    return (
+      <div>
+        <h1>HELLO THERE, MAREK.</h1>
+      </div>
+    )
+  }
+}
