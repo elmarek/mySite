97dba9e47cd9497754e02ef2717794e9e6b13d9a
elmarek <mareks@gmx.com>
2020-12-03 11:53:55 -0700

    Updated packageJSON

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-require
+const express = require("express");;
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+app.use(express.static(path.join(__dirname, "./dist")));
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function () {
+  console.log(`listening on port ${PORT}, player!`);
+});
